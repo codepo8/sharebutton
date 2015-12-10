0fdfef29ff593136aa7a9b1899a70e326abe063d
Chris Heilmann <chris.heilmann@gmail.com>
2015-12-10 17:24:18 -0500

    adding files

---
+++ sharebutton.js
@@ -0,0 +1,24 @@
+sharebutton = function() {
+  var config = {
+    hashtags: '',
+    link: '',
+    account: '',
+  }
+  function set(elm, text) {
+    var url = 'https://twitter.com/intent/tweet';
+    url += '?text=' + encodeURIComponent(text);
+    if (config.hashtags.length > 0) {
+      var hashtags = config.hashtags.split(',');
+      console.log('x');
+      url += encodeURIComponent(' #' + hashtags.join(' #'));
+    }
+    if (config.account !== '') {
+      url += '&via=' + encodeURIComponent(config.account);
+    }
+    if (config.link !== '') {
+      url += '&url=' + encodeURIComponent(config.link);
+    }
+    document.querySelector(elm).href = url;
+  }
+  return {set:set, config:config}
+}();
